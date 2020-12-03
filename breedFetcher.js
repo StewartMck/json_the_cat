0bbee2a43600b3b2b5bd36cce3e9dd0162058928
Stewart McKinlay <stewart.mckinlay@gmail.com>
2020-12-03 13:08:55 +0000

    initial commit of breedFetcher

---
+++ breedFetcher.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const breed = process.argv[2].slice(0, 4);
+
+request(("https://api.thecatapi.com/v1/images/search?breed_ids=" + breed), (error, response, body) => {
+  
+  if (!error) {
+    const data = (JSON.parse(body));
+    if (!data.length) {
+      return console.log('The breed was not found, check your spelling');
+    }
+    const adjustedData = Object.assign({}, data[0]["breeds"][0]);
+    console.log(adjustedData.description);
+  } else {
+    return console.log('ERROR:', error);
+  }
+  
+});
+
